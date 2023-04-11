10af79f6f6f3354b9bb450d95fad1dea3d52e84f
Your Name <you@example.com>
2023-04-11 17:45:36 +0200

    second commit

--- index.js
+++ index.js
@@ -13,19 +13,30 @@ const name = `${chalk.magenta(first)} ${chalk.magenta(last)}`
 
 console.log('name', name)
 
-console.log(`npm & node: ${process.env.npm_config_user_agent}`)
+if (process.env.npm_config_user_agent) {
+  console.log(`npm & node: ${process.env.npm_config_user_agent}`)
+} else {
+  console.log('npm_config_user_agent is not defined.')
+}
 
 const argumentParser = new Command();
-argumentParser.option('--date <date>', 'Specify a date in yyyy-MM-dd format');
+argumentParser.option('--date <date>', 'Date in the format of yyyy-MM-dd');
 argumentParser.parse();
 
+let dateSentAsArgument;
 const dateStringSentAsArgument = argumentParser.date
-const dateSentAsArgument = parse(dateStringSentAsArgument, 'yyyy-MM-dd', new Date()) 
+
+if (dateStringSentAsArgument) {
+  dateSentAsArgument = parse(dateStringSentAsArgument, 'yyyy-MM-dd', new Date())
+} else {
+  dateSentAsArgument = new Date();
+}
+
 const currentDate = set(new Date(), {hours: 0, minutes: 0, seconds: 0, milliseconds: 0})
 
-console.log('idag', isToday(dateSentAsArgument))
-console.log('imorgon', isAfter(dateSentAsArgument, currentDate))
-console.log('förrigår', isBefore(dateSentAsArgument, currentDate))
+console.log('is today:', isToday(dateSentAsArgument))
+console.log('is tomorrow:', isAfter(dateSentAsArgument, currentDate))
+console.log('is two days ago:', isBefore(dateSentAsArgument, currentDate))
 
 const compareDates = (date1, date2) => {
   if (isBefore(date1, date2)) {
@@ -44,14 +55,19 @@ console.log('Current date and time:', formattedDate);
 
 const fileContent = `
 name: ${first} ${last}
-npm & node: ${process.env.npm_config_user_agent}
+${process.env.npm_config_user_agent ? `npm & node: ${process.env.npm_config_user_agent}` : ''}
 git version: ${gitVersion}
 current date and time: ${formattedDate}
 date sent as argument: ${dateSentAsArgument}
 date sent as argument is ${compareDates(dateSentAsArgument, currentDate)} the current date
 `;
 
-await fs.writeFile('index.md', fileContent);
+try {
+  await fs.writeFile('index.md', fileContent);
+  console.log('File written successfully.')
+} catch (err) {
+  console.error('Error writing file:', err);
+}
 
 const startOfCourse = new Date(2023, 0, 31)
-console.log(formatDistanceToNow(startOfCourse))
+console.log(formatDistanceToNow(startOfCourse, { addSuffix: true }));
--- index.md
+++ index.md
@@ -2,6 +2,6 @@
 name: bella Ibrahimsson
 npm & node: npm/9.5.0 node/v19.7.0 win32 x64 workspaces/false
 git version: git version 2.40.0.windows.1
-current date and time: [96m2023-03-22 20:40:14[39m
-date sent as argument: Invalid Date
-date sent as argument is the same as the current date
+current date and time: [96m2023-04-11 17:34:54[39m
+date sent as argument: Tue Apr 11 2023 17:34:54 GMT+0200 (centraleuropeisk sommartid)
+date sent as argument is after the current date
